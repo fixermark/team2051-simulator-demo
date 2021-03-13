fe6da5d984a533b0f6dd4ea8f897f902bff7d6cf
Mark Tomczak <fixermark@gmail.com>
2021-03-13 18:18:37 -0500

    PID controller to regulate the turn motion. ~Markt

--- src/main/java/frc/robot/commands/Turn.java
+++ src/main/java/frc/robot/commands/Turn.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Hardware;
@@ -8,8 +9,10 @@ public class Turn extends CommandBase {
     private Hardware m_hardware;
     private DifferentialDrive m_drive;
     private double m_angle;
-    private double m_targetAngle;
-    private boolean m_turningRight;
+    private PIDController m_controller = new PIDController(0.1, 0.001, -.001);
+
+    private static final double MAX_SPEED = 1.0;
+    private static final double TOLERANCE = 0.5;
 
     /**
      * Create the command
@@ -21,20 +24,19 @@ public class Turn extends CommandBase {
         m_hardware = hardware;
         m_drive = drive;
         m_angle = angle;
-        m_turningRight = angle > 0;
+        m_controller.setTolerance(TOLERANCE);
     }
 
     @Override public void initialize() {
-        m_targetAngle = m_hardware.gyro().getAngleZ() + m_angle;
+        m_controller.reset();
+        m_controller.setSetpoint(m_hardware.gyro().getAngleZ() + m_angle);
     }
 
     @Override public void execute() {
-        double speed = 0.35;
-        if (m_turningRight) {
-            m_drive.tankDrive(speed, -speed);
-        } else {
-            m_drive.tankDrive(-speed, speed);
-        }
+        double output = m_controller.calculate(m_hardware.gyro().getAngleZ());
+        double clampedOutput = Math.max(-MAX_SPEED, Math.min(MAX_SPEED, output));
+        // positive rotations are clockwise, so should turn to the right
+        m_drive.tankDrive(clampedOutput, -clampedOutput);
     }
 
     @Override public void end(boolean interrupt) {
@@ -42,11 +44,6 @@ public class Turn extends CommandBase {
     }
 
     @Override public boolean isFinished() {
-        double angleZ = m_hardware.gyro().getAngleZ();
-        if (m_turningRight) {
-            return angleZ > m_targetAngle;
-        } else {
-            return angleZ < m_targetAngle;
-        }
+        return m_controller.atSetpoint();
     }
 }
